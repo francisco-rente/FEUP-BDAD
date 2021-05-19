0760baf9e381a6657e60169d5434436f8f8d5e4d
Tomás Vicente <up201904609@fe.up.pt>
2021-05-19 22:24:54 +0100

    Removed worst solution for int7.sql. Improve best solution.

--- Delivery_III/int7.sql
+++ Delivery_III/int7.sql
@@ -1,113 +1,39 @@
 /*
-FIXME: Important note: Two proposed solutions. Further comparison is required.
-
-When this query was designed, the idea was to consider which (if any) support
-requests could be given with the available resources (money and beds). However,
-a few aspects must be taken into account:
-- There is a giant disparity between the received and spent funds, to the point
-where any support request is automatically possible and testing is hard.
-- An assumption was made regarding the value of the monetary support (800 -
-salary), which may not make sense. It may be wiser to adapt the string to also
-include an amount.
-- Listing the support requests may not be that useful. After all, the table may
-become empty right after the first one is assigned. A better way could be all
-of the groups of support requests that may be given, but that would possibly be
-hard to implement and read.
-- Regardless, it is important to adapt the dataset in order to allow for more
-realistic scenarios.
-- This query could become just a statistic of the available resources, but that
-could make it too similar to existing ones.
-- This query could be extended to also include material support requests, which
-would make it necessary to know which product is being requested.
-*/
-
-/*
 Em relação aos pedidos de apoio pendentes, é importante saber quais deles
 podem ser satisfeitos com os recursos existentes. Assim, as camas restantes
 devem ser comparadas com os pedidos de alojamento e o saldo monetário deve ser
 comparado com os pedidos de apoio monetários.
  */
 
-SELECT PA.*
-FROM PedidoApoio PA
-         JOIN Necessitado N ON PA.pedinte = N.id
-         CROSS JOIN
-     (
-         SELECT *
-         FROM (
-                  SELECT (valorTotalRecebido - valorTotalGasto) AS saldoDisponivel
-                  FROM (
-                        (
-                            SELECT SUM(DM.valor) AS valorTotalRecebido
-                            FROM DoacaoMonetaria DM
-                        )
-                           CROSS JOIN
-                       (
-                           SELECT SUM(AM.valor) AS valorTotalGasto
-                           FROM ApoioMonetario AM
-                       )
-                           )
-              )
-                  CROSS JOIN
-              (
-                  SELECT (totalCamas - totalCamasOcupadas) AS totalCamasDisponiveis
-                  FROM (
-                        (
-                            SELECT COUNT(*) AS totalCamasOcupadas
-                            FROM ApoioAlojamento APA
-                                     JOIN Apoio AP ON AP.id = APA.id
-                            WHERE dataFim > DATE())
-                           CROSS JOIN
-                       (
-                           SELECT SUM(numeroCamas) AS totalCamas
-                           FROM Abrigo A
-                       )
-                           )
-              )
-     )
-WHERE (
-              (
-                      PA.tipo LIKE 'Alojamento'
-                      AND totalCamasDisponiveis > 0
-                  )
-              OR
-              (
-                      PA.tipo LIKE 'Monetário'
-                      AND (800 - N.rendimento) < saldoDisponivel
-                  )
-          );
-
 -- Selecionar pedidos de alojamento possíveis de satisfazer
 SELECT PA.*
 FROM PedidoApoio PA
-         CROSS JOIN
-     (
-         -- Procurar número de camas disponíveis
-         SELECT (totalCamas - totalCamasOcupadas) AS totalCamasDisponiveis
-         FROM (
-               (
-                   -- O total de camas ocupadas é igual ao total de apoios
-                   -- atribuídos que ainda não terminaram
-                   SELECT COUNT(*) AS totalCamasOcupadas
-                   FROM ApoioAlojamento APA
-                            JOIN Apoio AP ON AP.id = APA.id
-                        -- Considerar apenas os apoios que ainda não terminaram
-                   WHERE AP.dataFim > DATE()
-               )
-                  CROSS JOIN
-              (
-                  -- O total de camas é a soma das camas de todos os abrigos.
-                  SELECT SUM(A.numeroCamas) AS totalCamas
-                  FROM Abrigo A
-              )
-                  )
-              -- Selecionar pedidos apenas quando houver camas disponíveis.
-              -- Ao colocar aqui esta condição, evita-se fazer
-              -- todos os produtos cartesianos
-         WHERE (totalCamasDisponiveis > 0)
-     )
--- Apenas são relevantes os pedidos de alojamento
+     -- Apenas são relevantes os pedidos de alojamento
 WHERE tipo LIKE 'Alojamento'
+  -- Selecionar pedidos apenas quando houver camas disponíveis.
+  AND EXISTS(
+    -- Procurar número de camas disponíveis
+        SELECT (totalCamas - camasOcupadas) AS camasDisponiveis
+        FROM (
+              (
+                  -- O total de camas ocupadas é igual ao total de apoios
+                  -- atribuídos que ainda não terminaram
+                  SELECT COUNT(*) AS camasOcupadas
+                  FROM ApoioAlojamento APA
+                           JOIN Apoio AP ON AP.id = APA.id
+                       -- Considerar apenas os apoios que ainda não terminaram
+                  WHERE AP.dataFim > DATE()
+              )
+                 CROSS JOIN
+             (
+                 -- O total de camas é a soma das camas de todos os abrigos.
+                 SELECT SUM(A.numeroCamas) AS totalCamas
+                 FROM Abrigo A
+             )
+                 )
+             -- Devolver apenas quando houver camas disponíveis.
+        WHERE (camasDisponiveis > 0)
+    )
 
 UNION
 
@@ -121,17 +47,17 @@ FROM PedidoApoio PA
          -- Procurar saldo disponível para a organização
          SELECT *
          FROM (
-                  SELECT (valorTotalRecebido - valorTotalGasto) AS saldoDisponivel
+                  SELECT (valorRecebido - valorGasto) AS valorDisponivel
                   FROM (
                         (
                             -- O valor total recebido é a soma das doações
-                            SELECT SUM(DM.valor) AS valorTotalRecebido
+                            SELECT SUM(DM.valor) AS valorRecebido
                             FROM DoacaoMonetaria DM
                         )
                            CROSS JOIN
                        (
                            -- O valor total gasto é a soma dos apoios monetários atribuídos
-                           SELECT SUM(AM.valor) AS valorTotalGasto
+                           SELECT SUM(AM.valor) AS valorGasto
                            FROM ApoioMonetario AM
                        )
                            )
@@ -139,6 +65,6 @@ FROM PedidoApoio PA
               -- Selecionar pedidos apenas quando houver saldo disponível.
               -- Ao colocar aqui esta condição, evita-se fazer
               -- todos os produtos cartesianos
-         WHERE (saldoDisponivel > 0)
+         WHERE (valorDisponivel > 0)
      )
-WHERE (PA.tipo LIKE 'Monetário' AND (800 - N.rendimento) < saldoDisponivel);
+WHERE (PA.tipo LIKE 'Monetário' AND (800 - N.rendimento) < valorDisponivel);
