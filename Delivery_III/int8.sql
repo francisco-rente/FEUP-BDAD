ca0dea53f8178b5b4680850467453b45af148858
Tomás Vicente <up201904609@fe.up.pt>
2021-05-20 13:31:16 +0100

    Condensate int8.sql.

--- Delivery_III/int8.sql
+++ Delivery_III/int8.sql
@@ -53,3 +53,20 @@ FROM (
                   WHERE diasAteSeguinte <= 13
               )
      );
+
+SELECT SUM(valor * (1 + floor((13 - diasAteSeguinte) / frequencia)))
+           AS totalEsperado
+FROM (
+         SELECT valor,
+                frequencia,
+                FLOOR(
+                            JULIANDAY(DATE(JULIANDAY(MAX(data)) + frequencia)) -
+                            JULIANDAY()
+                    )
+                    AS diasAteSeguinte
+         FROM DoacaoMonetaria DM
+         WHERE pessoa NOT NULL
+           AND frequencia > 0
+         GROUP BY pessoa)
+WHERE diasAteSeguinte >= 0
+  AND diasAteSeguinte <= 13;
