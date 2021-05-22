ac0248bd975db67be5f28e6483365a2ff572aa8e
Tomás Vicente <up201904609@fe.up.pt>
2021-05-22 09:57:30 +0100

    Comments.

--- Delivery_III/Trigger2/gatilho2_adiciona.sql
+++ Delivery_III/Trigger2/gatilho2_adiciona.sql
@@ -20,6 +20,8 @@ CREATE TRIGGER trg2
     FOR EACH ROW
 BEGIN
     DELETE
+        -- Nota: Devido às regras ON DELETE, esta entrada será também removida da
+        -- tabela produto, pelo que não é necessário fazê-lo manualmente.
     FROM ProdutoAlimentar
     WHERE ProdutoAlimentar.dataValidade < DATE();
 END;
