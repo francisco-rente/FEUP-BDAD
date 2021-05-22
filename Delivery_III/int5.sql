c63f9957402a22cc4b62fe7fbb6517d9f5c568d6
Tomás Vicente <up201904609@fe.up.pt>
2021-05-22 10:19:18 +0100

    Reformat file.

--- Delivery_III/int5.sql
+++ Delivery_III/int5.sql
@@ -25,40 +25,47 @@ FROM (
                 infoNecessitados.rendimentoMedio AS rendimentoMedio
          FROM Abrigo AB
                   INNER JOIN
-              (SELECT AB.id, PAP.justificacao, COUNT(*) AS jCount
-               FROM Abrigo AB
-                        INNER JOIN ApoioAlojamento APA ON AB.id = APA.abrigo
-                        INNER JOIN Apoio AP ON APA.id = AP.id
-                        INNER JOIN PedidoApoio PAP ON AP.pedido = PAP.id
-               WHERE AP.dataFim >= DATE('now')
-               GROUP BY AB.id, PAP.justificacao) infoJustificacoes
+              (
+                  SELECT AB.id, PAP.justificacao, COUNT(*) AS jCount
+                  FROM Abrigo AB
+                           INNER JOIN ApoioAlojamento APA ON AB.id = APA.abrigo
+                           INNER JOIN Apoio AP ON APA.id = AP.id
+                           INNER JOIN PedidoApoio PAP ON AP.pedido = PAP.id
+                  WHERE AP.dataFim >= DATE('now')
+                  GROUP BY AB.id, PAP.justificacao) infoJustificacoes
               ON AB.id = infoJustificacoes.id
                   INNER JOIN
-              (SELECT AB.id, Pais.nome, COUNT(*) AS pCount
-               FROM Abrigo AB
-                        INNER JOIN ApoioAlojamento APA ON AB.id = APA.abrigo
-                        INNER JOIN Apoio AP ON APA.id = AP.id
-                        INNER JOIN PedidoApoio PAP ON AP.pedido = PAP.id
-                        INNER JOIN Necessitado NES ON PAP.pedinte = NES.id
-                        INNER JOIN Pessoa PES ON NES.id = PES.id
-                   -- JOIN Pessoa PES ON PAP.pedinte = PES.id
-                        INNER JOIN Localidade LOC ON PES.codigoZona = LOC.codigo
-                        INNER JOIN Pais ON LOC.codigoPais = Pais.codigo
-               WHERE AP.dataFim >= DATE('now')
-               GROUP BY AB.id, Pais.codigo) infoNacionalidades
+              (
+                  SELECT AB.id, Pais.nome, COUNT(*) AS pCount
+                  FROM Abrigo AB
+                           INNER JOIN ApoioAlojamento APA ON AB.id = APA.abrigo
+                           INNER JOIN Apoio AP ON APA.id = AP.id
+                           INNER JOIN PedidoApoio PAP ON AP.pedido = PAP.id
+                           INNER JOIN Necessitado NES ON PAP.pedinte = NES.id
+                           INNER JOIN Pessoa PES ON NES.id = PES.id
+                      -- JOIN Pessoa PES ON PAP.pedinte = PES.id
+                           INNER JOIN Localidade LOC ON PES.codigoZona = LOC.codigo
+                           INNER JOIN Pais ON LOC.codigoPais = Pais.codigo
+                  WHERE AP.dataFim >= DATE('now')
+                  GROUP BY AB.id, Pais.codigo
+              ) infoNacionalidades
               ON AB.id = infoNacionalidades.id
                   INNER JOIN
-              (SELECT AB.id,
-                      AVG(NES.rendimento) AS rendimentoMedio,
-                      floor(AVG(JULIANDAY() - JULIANDAY(PES.dataNascimento)) /
-                            365)          AS idadeMedia
-               FROM Abrigo AB
-                        INNER JOIN ApoioAlojamento APA ON AB.id = APA.abrigo
-                        INNER JOIN Apoio AP ON APA.id = AP.id
-                        INNER JOIN PedidoApoio PAP ON AP.pedido = PAP.id
-                        INNER JOIN Necessitado NES ON PAP.pedinte = NES.id
-                        INNER JOIN Pessoa PES ON NES.id = PES.id
-               WHERE AP.dataFim >= DATE()
-               GROUP BY AB.id) infoNecessitados ON AB.id = infoNecessitados.id
+              (
+                  SELECT AB.id,
+                         AVG(NES.rendimento) AS rendimentoMedio,
+                         floor(AVG(
+                                       JULIANDAY() - JULIANDAY(PES.dataNascimento)) /
+                               365)          AS idadeMedia
+                  FROM Abrigo AB
+                           INNER JOIN ApoioAlojamento APA ON AB.id = APA.abrigo
+                           INNER JOIN Apoio AP ON APA.id = AP.id
+                           INNER JOIN PedidoApoio PAP ON AP.pedido = PAP.id
+                           INNER JOIN Necessitado NES ON PAP.pedinte = NES.id
+                           INNER JOIN Pessoa PES ON NES.id = PES.id
+                  WHERE AP.dataFim >= DATE()
+                  GROUP BY AB.id
+              ) infoNecessitados
+              ON AB.id = infoNecessitados.id
          GROUP BY AB.id
      );
