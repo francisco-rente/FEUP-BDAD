df92df45503c2832b48ac52babe0e6911b286240
Tomás Vicente <up201904609@fe.up.pt>
2021-05-22 11:23:14 +0100

    Remove percentage (did not work).

--- Delivery_III/int5.sql
+++ Delivery_III/int5.sql
@@ -6,20 +6,18 @@
 -- seus habitantes: média de idades, média de rendimentos, nacionalidade mais
 -- comum e justificação mais comum para requerer asilo.
 
-SELECT id,
-       justificacaoMaisComum,
-       percentagemJustificacoes,
-       paisMaisComum,
-       percentagemPais,
-       idadeMedia,
-       rendimentoMedio
+SELECT id                    AS Abrigo,
+       justificacaoMaisComum AS 'Justificacao mais comum',
+       nJustificacoes        AS 'N Justificacoes',
+       paisMaisComum         AS 'Nacionalidade Mais Comum',
+       nPais                 AS 'N. Nacionais',
+       idadeMedia            AS 'Idade media',
+       rendimentoMedio       AS 'Rendimento medio'
 FROM (
          SELECT AB.id,
-                100 * MAX(infoJustificacoes.jCount) /
-                SUM(infoJustificacoes.jCount)    AS percentagemJustificacoes,
+                MAX(infoJustificacoes.jCount)    AS nJustificacoes,
                 infoJustificacoes.justificacao   AS justificacaoMaisComum,
-                100 * MAX(infoNacionalidades.pCount) /
-                SUM(infoNacionalidades.pCount)   AS percentagemPais,
+                MAX(infoNacionalidades.pCount)   AS nPais,
                 infoNacionalidades.nome          AS paisMaisComum,
                 infoNecessitados.idadeMedia      AS idadeMedia,
                 infoNecessitados.rendimentoMedio AS rendimentoMedio
@@ -31,8 +29,9 @@ FROM (
                            INNER JOIN ApoioAlojamento APA ON AB.id = APA.abrigo
                            INNER JOIN Apoio AP ON APA.id = AP.id
                            INNER JOIN PedidoApoio PAP ON AP.pedido = PAP.id
-                  WHERE AP.dataFim >= DATE('now')
-                  GROUP BY AB.id, PAP.justificacao) infoJustificacoes
+                  WHERE AP.dataFim >= DATE() -- Apenas apoios ativos
+                  GROUP BY AB.id, PAP.justificacao
+              ) infoJustificacoes
               ON AB.id = infoJustificacoes.id
                   INNER JOIN
               (
@@ -41,12 +40,11 @@ FROM (
                            INNER JOIN ApoioAlojamento APA ON AB.id = APA.abrigo
                            INNER JOIN Apoio AP ON APA.id = AP.id
                            INNER JOIN PedidoApoio PAP ON AP.pedido = PAP.id
-                           INNER JOIN Necessitado NES ON PAP.pedinte = NES.id
-                           INNER JOIN Pessoa PES ON NES.id = PES.id
-                      -- JOIN Pessoa PES ON PAP.pedinte = PES.id
+                      -- Generalização: Necessitado.id/PAP.pedinte = Pessoa.id
+                           INNER JOIN Pessoa PES ON PAP.pedinte = PES.id
                            INNER JOIN Localidade LOC ON PES.codigoZona = LOC.codigo
                            INNER JOIN Pais ON LOC.codigoPais = Pais.codigo
-                  WHERE AP.dataFim >= DATE('now')
+                  WHERE AP.dataFim >= DATE() -- Apenas apoios ativos
                   GROUP BY AB.id, Pais.codigo
               ) infoNacionalidades
               ON AB.id = infoNacionalidades.id
@@ -54,9 +52,11 @@ FROM (
               (
                   SELECT AB.id,
                          AVG(NES.rendimento) AS rendimentoMedio,
-                         floor(AVG(
-                                       JULIANDAY() - JULIANDAY(PES.dataNascimento)) /
-                               365)          AS idadeMedia
+                         floor(
+                                     AVG(
+                                             JULIANDAY() - JULIANDAY(PES.dataNascimento)) -- Dias desde o nascimento
+                                     / 365 -- Anos desde o nascimento
+                             )               AS idadeMedia
                   FROM Abrigo AB
                            INNER JOIN ApoioAlojamento APA ON AB.id = APA.abrigo
                            INNER JOIN Apoio AP ON APA.id = AP.id
